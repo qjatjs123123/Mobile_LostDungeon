f4aa297b2033f0b23c7827893e988a8baf8fee31
Hong Boem Sun <qjatjs123123@naver.com>
2022-02-06 21:11:12 +0900

    조이스틱 카메라구현 핸드폰연결테스트시연성공

---
+++ LostDungeon/Assets/Script/MainController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody rigibody;
+
+    private float h;
+    private float v;
+
+    private float moveX;
+    private float moveZ;
+    private float speedH = 100f;
+    private float speedZ = 160f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigibody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            animator.Play("JUMP00", -1, 0);
+
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        animator.SetFloat("h", h);
+        animator.SetFloat("v", v);
+
+        moveX = h * speedH * Time.deltaTime;
+        moveZ = v * speedZ * Time.deltaTime;
+
+
+
+        rigibody.velocity = new Vector3(moveX, 0, moveZ);
+    }
+}
---
+++ LostDungeon/Assets/Script/VirtualjoyStick.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; //키보드, 마우스, 터치를 이벤트로 오브젝트에 보낼 수 있음
+
+public class VirtualjoyStick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    private RectTransform lever;
+    private RectTransform rectTransform;
+
+    [SerializeField, Range(10,150)]
+    private float leverRange;
+
+    private Vector2 inputDirection;
+    private bool isInput;
+
+    [SerializeField]
+    private TPSCharacterController controller;
+
+    public enum JoystickType { Move, Rotate}
+    public JoystickType joystickType;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        ControlJoyStickLever(eventData);
+        isInput = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        ControlJoyStickLever(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        lever.anchoredPosition = Vector2.zero;
+        isInput = false;
+        switch (joystickType)
+        {
+            case JoystickType.Move:
+                controller.Move(inputDirection);
+                break;
+
+            case JoystickType.Rotate:
+                controller.LookAround(inputDirection*0.8f);
+                break;
+        }
+    }
+
+    private void ControlJoyStickLever(PointerEventData eventData)
+    {
+        var inputPos = eventData.position - rectTransform.anchoredPosition;
+        var inputVector = inputPos.magnitude < leverRange ? inputPos : inputPos.normalized * leverRange;
+        lever.anchoredPosition = inputVector;
+        inputDirection = inputVector / leverRange;
+    }
+
+    private void InputControlVector()
+    {
+        switch (joystickType)
+        {
+            case JoystickType.Move:
+                controller.Move(inputDirection);
+                break;
+
+            case JoystickType.Rotate:
+                controller.LookAround(inputDirection*0.8f);
+                break;
+        }
+       
+        //Debug.Log(inputDirection.x + "/" + inputDirection.y);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInput)
+            InputControlVector();
+    }
+}
