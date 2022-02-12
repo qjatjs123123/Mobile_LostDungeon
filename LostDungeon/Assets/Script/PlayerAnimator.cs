5cc4e086bf9715b4db5cd519a02887364ee594e9
Hong Boem Sun <qjatjs123123@naver.com>
2022-02-12 14:20:27 +0900

    무기 공격 애니메이션 구현

---
+++ LostDungeon/Assets/Script/PlayerAnimator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField]
+    private Transform characterBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        animator = characterBody.GetComponent<Animator>();
+    }
+
+    public void OnWeaponAttack()
+    {
+        
+        animator.SetTrigger("OnWeaponAttack");
+    }
+}
